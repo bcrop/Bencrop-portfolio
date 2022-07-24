ecf44f75f563f3d52bac11617487bfca6a237932
bcrop <45927313+bcrop@users.noreply.github.com>
2022-07-23 20:45:43 -0700

    livestream link and date update

--- js/simplyCountdown.js
+++ js/simplyCountdown.js
@@ -118,7 +118,7 @@
                 year: 2022,
                 month: 7,
                 day: 30,
-                hours: 9,
+                hours: 10,
                 minutes: 0,
                 seconds: 0,
                 words: {
